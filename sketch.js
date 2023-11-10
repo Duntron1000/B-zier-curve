55fe52adb73cf2f4edc1fc868d137757b92a8135
Duntron1000 <90113909+Duntron1000@users.noreply.github.com>
2023-11-09 21:12:07 -0500

    better and with 4 control points

--- sketch.js
+++ sketch.js
@@ -1,61 +1,89 @@
-const x1 = 50;
-const x2 = 200;
-const y1 = 200;
-const y2 = 100;
-const x3 = 350;
-const y3 = 200;
-let t = 0;
-let back = false;
+let layers = [[], [], [], []];
+let bs = [];
 let px = [];
 let py = [];
+let controlNodes = [];
+let derivedNodes = [];
+let thirdNodes = [];
+let t = 0;
+let back = false;
 let c;
+bs.push(new besier(layers[0]));
+bs.push(new besier(layers[1]));
+bs.push(new besier(layers[2]));
+bs.push(new besier(layers[3]));
 
 function setup() {
-  c = color(0, 0, 0);
+  c = color(random(255), random(255), random(255));
+
   createCanvas(400, 400);
+  layers[0].push(new dot(50, 200, 10));
+  layers[0].push(new dot(200, 300, 10));
+  layers[0].push(new dot(330, 100, 10));
+  layers[0].push(new dot(350, 200, 10));
+  for (let i = 0; i <= 2; i++) {
+    layers[1].push(new dot(0, 0, 15));
+  }
+  for (let i = 0; i <= 1; i++) {
+    layers[2].push(new dot(0, 0, 15));
+  }
+  layers[3].push(new dot(0, 0, 15));
 }
 
 function draw() {
   background(220);
-  line(x1, y1, x2, y2);
-  line(x2, y2, x3, y3);
-  ellipse(x1, y1, 10, 10);
-  ellipse(x2, y2, 10, 10);
-  ellipse(x3, y3, 10, 10);
-  l1x = lerp(x1, x2, t);
-  l1y = lerp(y1, y2, t);
-  l2x = lerp(x2, x3, t);
-  l2y = lerp(y2, y3, t);
-  l3x = lerp(l1x, l2x, t);
-  l3y = lerp(l1y, l2y, t);
-  line(l1x, l1y, l2x, l2y);
-  px.push(l3x);
-  py.push(l3y);
+  for (const c of bs) {
+    c.disp();
+  }
+
+  for (let i = 0; i < layers[0].length - 1; i++) {
+    layers[1][i].updatePos(
+      lerp(layers[0][i].x, layers[0][i + 1].x, t),
+      lerp(layers[0][i].y, layers[0][i + 1].y, t)
+    );
+  }
+
+  layers[2][0].updatePos(
+    lerp(layers[1][0].x, layers[1][1].x, t),
+    lerp(layers[1][0].y, layers[1][1].y, t)
+  );
+
+  layers[2][1].updatePos(
+    lerp(layers[1][1].x, layers[1][2].x, t),
+    lerp(layers[1][1].y, layers[1][2].y, t)
+  );
+
+  px.push(lerp(layers[2][0].x, layers[2][1].x, t));
+  py.push(lerp(layers[2][0].y, layers[2][1].y, t));
+
+  layers[3][0].updatePos(
+    lerp(layers[2][0].x, layers[2][1].x, t),
+    lerp(layers[2][0].y, layers[2][1].y, t)
+  );
+
   stroke(c);
   fill(c);
   for (let i = 0; i < px.length; i++) {
     ellipse(px[i], py[i], 5, 5);
   }
   stroke(0);
-  fill(255);
-  ellipse(l3x, l3y, 20, 20);
-  ellipse(l1x, l1y, 20, 20);
-  ellipse(l2x, l2y, 20, 20);
+  fill(0);
 
   if (t >= 1) {
     back = true;
     px.length = 0;
     py.length = 0;
     c = color(random(255), random(255), random(255));
-  } else if (t <= 0) {
+  }
+  if (t <= 0) {
     back = false;
     px.length = 0;
     py.length = 0;
     c = color(random(255), random(255), random(255));
   }
   if (back) {
-    t -= 0.005;
+    t -= 0.003;
   } else {
-    t += 0.005;
+    t += 0.003;
   }
 }
